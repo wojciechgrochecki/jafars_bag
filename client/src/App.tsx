1cdc68bccbebd95044b8660ec915ad5817cbd9e9
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-04 18:04:26 +0200

    feat(routing): add routing to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,16 @@
+import MobileNav from "./components/MobileNav/MobileNav";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+
 function App() {
-  return <div className="h-20  w-20 bg-red-100"></div>;
+  return (
+    <div className="relative h-fit min-h-screen w-full bg-slate-50">
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+      </Routes>
+      <MobileNav />
+    </div>
+  );
 }
 
 export default App;
