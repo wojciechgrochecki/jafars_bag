cfc4f9b0e5c1c129e3723614b68bdfaacfce69c9
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-04 15:09:16 +0200

    feat(mobile-nav): create `MobileNav` component

---
+++ client/src/components/MobileNav/MobileNav.tsx
@@ -0,0 +1,16 @@
+import AddOfferButton from "../buttons/AddOfferButton";
+import { BellIcon, HomeIcon, SearchIcon, UserIcon } from "./NavIcons";
+
+function MobileNav() {
+  return (
+    <nav className=" absolute bottom-6 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] bg-white px-3 py-3 shadow-nav">
+      <HomeIcon active />
+      <SearchIcon />
+      <AddOfferButton />
+      <BellIcon />
+      <UserIcon />
+    </nav>
+  );
+}
+
+export default MobileNav;
