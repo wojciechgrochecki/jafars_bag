47b403b6c1863b0979befa5cac565dd3d1b8881e
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-05 01:02:31 +0200

    feat(tailwind-config): extend utility classes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,8 +5,11 @@ export default {
     extend: {
       boxShadow: {
         nav: "0px 2px 5px 0px rgba(15, 23, 42, 0.18)",
+        ring: "0px 0px 0px 1px rgba(9, 30, 66, 0.08), 0px 4px 8px -6px rgba(9, 30, 66, 0.25)",
+        "ring-accent":
+          "0px 0px 0px 1px rgba(133, 53, 204, 0.2),0px 4px 8px -6px rgba(133, 53, 204, 0.25)",
       },
-      colors: { accent: "#8635CC" },
+      colors: { accent: "#8635CC", "accent-light": "#9352CC" },
     },
   },
   plugins: [],
