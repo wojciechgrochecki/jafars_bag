8c4bc7a39f08869cf0a7589724665f31aca4476d
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-04 15:07:20 +0200

    feat(tailwind): add utility classes

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        nav: "0px 2px 5px 0px rgba(15, 23, 42, 0.18)",
+      },
+      colors: { accent: "#8635CC" },
+    },
   },
   plugins: [],
 };
