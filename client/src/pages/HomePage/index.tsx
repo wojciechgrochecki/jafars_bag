491463fa5ae6a082811a1c7da29a87911f3d6850
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-05 01:08:54 +0200

    feat(new-page): create `HomePage` page

---
+++ client/src/pages/HomePage/index.tsx
@@ -0,0 +1,11 @@
+import OfferExcerpt from "../../components/Offer/OfferExcerpt";
+
+function HomePage() {
+  return (
+    <main className="min-h-full w-full px-4 py-2">
+      <OfferExcerpt />
+    </main>
+  );
+}
+
+export default HomePage;
