5dda786de6109294602545d3ab7e36ef6e3f1ce9
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-05 15:58:15 +0200

    feat(typography): add `TypographyP` component

---
+++ client/src/components/Typography/TypographyP.tsx
@@ -0,0 +1,14 @@
+import { cn } from "../../utils/cn";
+
+interface Props {
+  className?: string;
+  children: React.ReactNode;
+}
+
+export default function TypographyP({ className, children }: Props) {
+  return (
+    <p className={cn("text-sm font-normal text-slate-600", className)}>
+      {children}
+    </p>
+  );
+}
