64d18c807fc089d8ebddd143e02454380869ce91
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-06 01:11:25 +0200

    feat(input): create reusable `Input` component

---
+++ client/src/components/forms/inputs/input/index.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+import { cn } from "../../../../utils/cn";
+
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-lg border border-slate-200 bg-white px-3 py-2 text-sm text-slate-700 caret-accent-light placeholder:text-slate-400 focus:border-accent-light focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          className,
+        )}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Input.displayName = "Input";
+
+export { Input };
