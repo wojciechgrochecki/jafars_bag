8711bb70f873a22e2720d0fab6da1ffee28414a7
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-05 13:10:07 +0200

    feat(button): create reusable button component

--- client/src/components/Offer/OfferExcerpt/index.tsx
+++ client/src/components/Offer/OfferExcerpt/index.tsx
@@ -1,6 +1,7 @@
 import TypographyH4 from "../../Typography/TypographyH4";
+// import AddToFavButton from "../../buttons/AddToFavButton";
+import { Button } from "../../buttons";
 import AddToFavButton from "../../buttons/AddToFavButton";
-import PrimaryButton from "../../buttons/PrimaryButton";
 import EndsIn from "../EndsIn";
 import PriceInfo from "../PriceInfo";
 import OfferImg from "./OfferImg";
@@ -8,7 +9,7 @@ import { IconLink } from "@tabler/icons-react";
 
 function OfferExcerpt() {
   return (
-    <div className="shadow-ring flex flex-col gap-3 rounded-lg p-3">
+    <div className="flex flex-col gap-3 rounded-lg p-3 shadow-ring">
       <div className="flex flex-row gap-2">
         <OfferImg />
         <div className="flex flex-col gap-1">
@@ -21,9 +22,9 @@ function OfferExcerpt() {
         </div>
       </div>
       <div className="flex flex-row gap-3">
-        <PrimaryButton className="w-full">
+        <Button className="w-full">
           Sprawdź okazję <IconLink className="h-5 w-5" />
-        </PrimaryButton>
+        </Button>
         <AddToFavButton active={false} />
       </div>
     </div>
---
+++ client/src/components/buttons/index.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import { cn } from "../../utils/cn";
+
+const buttonVariants = cva(
+  "flex flex-row items-center justify-center gap-1 rounded-md text-sm font-medium",
+  {
+    variants: {
+      variant: {
+        default: "bg-accent text-white",
+        secondary: "text-accent shadow-ring-accent bg-white",
+      },
+      size: {
+        default: "h-9 px-3 py-2",
+        icon: "h-9 w-9 flex-shrink-0",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  },
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, size, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  },
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
