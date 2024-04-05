61669c32ff06cf0ee68e26f9955e1e1d85e8c054
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-05 16:00:09 +0200

    style(MobileNav): change `absolute` position to `fixed`

--- client/src/components/Nav/MobileNav/index.tsx
+++ client/src/components/Nav/MobileNav/index.tsx
@@ -3,7 +3,7 @@ import { BellIcon, HomeIcon, SearchIcon, UserIcon } from "./NavIcons";
 
 function MobileNav() {
   return (
-    <nav className=" absolute bottom-6 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] bg-white px-3 py-3 shadow-nav">
+    <nav className="fixed bottom-3 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] bg-white px-3 py-3 shadow-nav">
       <HomeIcon active />
       <SearchIcon />
       <AddOfferButton />
