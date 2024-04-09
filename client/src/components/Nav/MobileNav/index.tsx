165fe59655ab79c962ecc957a6708970421b1cd6
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-09 23:16:21 +0200

    feat(mobile-nav): make `AddOfferButton` link to add deal form page

--- client/src/components/Nav/MobileNav/index.tsx
+++ client/src/components/Nav/MobileNav/index.tsx
@@ -6,12 +6,14 @@ function MobileNav() {
   return (
     <>
       <div className="h-24 w-full bg-transparent"></div>
-      <nav className="fixed bottom-3 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] bg-white px-3 py-3 shadow-nav">
+      <nav className="fixed bottom-3 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] border border-slate-200 bg-white px-3 py-3 shadow-nav">
         <NavLink to="/" end>
           {({ isActive }) => <HomeIcon active={isActive} />}
         </NavLink>
         <SearchIcon />
-        <AddOfferButton />
+        <NavLink to="/dodaj-ogloszenie" end className={"contents"}>
+          <AddOfferButton />
+        </NavLink>
         <BellIcon />
         <NavLink to="/konto">
           {({ isActive }) => <UserIcon active={isActive} />}
