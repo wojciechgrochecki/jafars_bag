0f57a2c452134e97f2883af2b43a89faf768778b
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-09 23:15:31 +0200

    feat(app-routes): add route to form for adding new deals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "@/pages/HomePage";
 import DealPage from "@/pages/DealPage";
 import LayoutWithNav from "./layouts/LayoutWithNav";
+import AddDealLayout from "./pages/AddDealPage/AddDealFormLayout";
+import LinkStep from "./pages/AddDealPage/LinkStep";
+import DetailsStep from "./pages/AddDealPage/DetailsStep";
 
 function App() {
   return (
@@ -11,6 +14,10 @@ function App() {
           <Route index path="/" element={<HomePage />} />
         </Route>
         <Route path="okazja/:id" element={<DealPage />} />
+        <Route path="dodaj-ogloszenie" element={<AddDealLayout />}>
+          <Route index element={<LinkStep />} />
+          <Route path={"szczegoly"} element={<DetailsStep />} />
+        </Route>
       </Routes>
     </>
   );
