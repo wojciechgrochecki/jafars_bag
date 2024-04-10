5df5ab316c3532560fef48e8b344ac1fcf6255ff
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-10 22:06:18 +0200

    feat(deal-form): create layout for `AddDealPage`

---
+++ client/src/pages/AddDealPage/AddDealFormLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+export default function AddDealFormLayout() {
+  return (
+    <div className="flex h-fit min-h-screen flex-col gap-8 bg-white px-4 py-3">
+      <div className="h-14 bg-slate-300">navigation/stepper</div>
+      <Outlet />
+    </div>
+  );
+}
