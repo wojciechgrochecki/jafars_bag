b3c63ddfb84e71353a3b052cad104e4ed1473913
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-11 01:52:33 +0200

    feat(textarea): make textarea auto-resizable if specified

--- client/src/components/forms/inputs/textarea/index.tsx
+++ client/src/components/forms/inputs/textarea/index.tsx
@@ -1,18 +1,32 @@
 import * as React from "react";
 import { cn } from "@/utils/cn";
+import mergeRefs from "merge-refs";
 
 export interface TextareaProps
-  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+  extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {
+  autoResize?: boolean;
+}
 
 const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
-  ({ className, ...props }, ref) => {
+  ({ className, autoResize = true, onChange, ...props }, ref) => {
+    const textAreaRef = React.useRef<HTMLTextAreaElement>(null);
+
+    const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+      if (textAreaRef.current && autoResize) {
+        textAreaRef.current.style.height = "auto";
+        textAreaRef.current.style.height = `${e.target.scrollHeight + 2}px`;
+      }
+      if (onChange) onChange(e);
+    };
+
     return (
       <textarea
         className={cn(
-          "flex w-full resize-none rounded-lg border border-slate-200 bg-white px-3 py-2 text-sm text-slate-700 caret-accent-light placeholder:text-slate-400 focus:border-accent-light focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-40 w-full resize-none rounded-lg border border-slate-200 bg-white px-3 py-2 text-sm text-slate-700 caret-accent-light placeholder:text-slate-400 focus:border-accent-light focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
-        ref={ref}
+        ref={mergeRefs(ref, textAreaRef)}
+        onChange={handleInput}
         {...props}
       />
     );
