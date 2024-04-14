cd1b3f94e161409c717ea3e4f0b947db9ac7c4d2
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-14 21:28:18 +0200

    feat(images-step): allow for images reordering

--- client/src/pages/AddDealPage/ImagesStep.tsx
+++ client/src/pages/AddDealPage/ImagesStep.tsx
@@ -1,12 +1,28 @@
 import TypographyH2 from "@/components/Typography/TypographyH2";
+import TypographyH4 from "@/components/Typography/TypographyH4";
 import { Button } from "@/components/buttons";
+import Draggable from "@/components/draggable/Draggable";
 import Dropzone from "@/components/forms/inputs/dropzone";
+import { ImagePreview } from "@/components/forms/inputs/dropzone/ImagePreview";
 import { useState } from "react";
+import { SortableEvent } from "sortablejs";
 
 export default function ImagesStep() {
   const [files, setFiles] = useState<File[]>([]);
+
+  const handleDelete = (fileName: string) => {
+    setFiles((prev) => prev?.filter((file) => file.name !== fileName));
+  };
+
+  const handleDragEnd = ({ newIndex, oldIndex }: SortableEvent) =>
+    setFiles((prev) => {
+      const newOrder = [...prev];
+      newOrder.splice(newIndex!, 0, newOrder.splice(oldIndex!, 1)[0]);
+      return newOrder;
+    });
+
   return (
-    <div className="flex flex-col items-center gap-8 pb-20">
+    <div className="flex max-w-3xl flex-col items-center gap-8 pb-20">
       <header>
         <TypographyH2 className="text-center">Zdjęcia</TypographyH2>
         <p className="font-base mt-2 text-center text-slate-500">
@@ -16,10 +32,22 @@ export default function ImagesStep() {
       <main className="w-full">
         <form
           onSubmit={(e) => e.preventDefault()}
-          className="h-fit min-h-full space-y-6"
+          className="h-fit min-h-full space-y-4"
         >
           <Dropzone files={files} setFiles={setFiles} />
-
+          <div className="space-y-2">
+            <TypographyH4>Wybrane zdjęcia</TypographyH4>
+            <Draggable onEnd={handleDragEnd}>
+              {files.map((file, i) => (
+                <ImagePreview
+                  key={file.name}
+                  file={file}
+                  onDelete={handleDelete}
+                  leading={i === 0}
+                />
+              ))}
+            </Draggable>
+          </div>
           <div className="fixed bottom-0 left-0 z-10 w-full border border-slate-300 bg-white p-4">
             <Button type="submit" className="h-fit w-full py-3">
               Następny krok
