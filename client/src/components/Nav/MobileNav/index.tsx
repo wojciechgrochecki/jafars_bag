157a901c4936fbfdfcebc45989581bfc9ff59b11
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-16 18:53:14 +0200

    style(mobile-nav): remove space between nav and bottom of screen

--- client/src/components/Nav/MobileNav/index.tsx
+++ client/src/components/Nav/MobileNav/index.tsx
@@ -6,13 +6,13 @@ function MobileNav() {
   return (
     <>
       <div className="h-24 w-full bg-transparent"></div>
-      <nav className="fixed bottom-3 left-3 right-3 flex h-16 flex-row items-center justify-between rounded-[20px] border border-slate-200 bg-white px-3 py-3 shadow-nav">
+      <nav className="fixed bottom-0 left-0 flex h-[70px] w-full flex-row items-center justify-between border-t border-accent-light/20 bg-white px-4 shadow-nav">
         <NavLink to="/" end>
           {({ isActive }) => <HomeIcon active={isActive} />}
         </NavLink>
         <SearchIcon />
         <NavLink to="/dodaj-ogloszenie" end className={"contents"}>
-          <AddOfferButton />
+          <AddOfferButton className="relative bottom-1/3 transition hover:scale-110" />
         </NavLink>
         <BellIcon />
         <NavLink to="/konto">
