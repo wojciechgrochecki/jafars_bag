190c583006cddcb3253fb4952f428b2d10655bb9
Wojciech Grochecki <wojciech5522@gmail.com>
2024-04-16 18:44:11 +0200

    feat(input): change input border based on `invalid` prop

--- client/src/components/forms/inputs/input/index.tsx
+++ client/src/components/forms/inputs/input/index.tsx
@@ -3,16 +3,19 @@ import * as React from "react";
 import { cn } from "@/utils/cn";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  invalid?: boolean;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, invalid, type, ...props }, ref) => {
     return (
       <input
         type={type}
         className={cn(
           "flex h-10 w-full rounded-lg border border-slate-200 bg-white px-3 py-2 text-sm text-slate-700 caret-accent-light placeholder:text-slate-400 focus:border-accent-light focus:outline-none disabled:cursor-not-allowed disabled:opacity-50",
           className,
+          { "border-red-600": invalid },
         )}
         ref={ref}
         {...props}
