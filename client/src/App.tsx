2b4b62c4b764414457d3b920c28149b8d5ef8719
Wojciech Grochecki <wojciech5522@gmail.com>
2024-05-22 10:45:46 +0200

    chore(app-routes): remove specific add deal form routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,10 +3,6 @@ import HomePage from "@/pages/HomePage";
 import DealPage from "@/pages/DealPage";
 import LayoutWithNav from "./layouts/LayoutWithNav";
 import FormLayout from "./pages/AddDealPage/FormLayout";
-import LinkStep from "./pages/AddDealPage/Steps/LinkStep";
-import DetailsStep from "./pages/AddDealPage/Steps/DetailsStep";
-import DescriptionStep from "./pages/AddDealPage/Steps/DescriptionStep";
-import ImagesStep from "./pages/AddDealPage/Steps/ImagesStep";
 
 function App() {
   return (
@@ -16,12 +12,7 @@ function App() {
           <Route index path="/" element={<HomePage />} />
         </Route>
         <Route path="okazja/:id" element={<DealPage />} />
-        <Route path="dodaj-ogloszenie" element={<FormLayout />}>
-          <Route index element={<LinkStep />} />
-          <Route path={"szczegoly"} element={<DetailsStep />} />
-          <Route path={"opis"} element={<DescriptionStep />} />
-          <Route path={"zdjecia"} element={<ImagesStep />} />
-        </Route>
+        <Route path="dodaj-ogloszenie/*" element={<FormLayout />} />
       </Routes>
     </>
   );
