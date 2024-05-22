b3760b74f510b615f2f45c8dcb80fc01f4468e1d
Wojciech Grochecki <wojciech5522@gmail.com>
2024-05-22 20:17:47 +0200

    feat(account-page): add options button

--- client/src/pages/AccountPage/index.tsx
+++ client/src/pages/AccountPage/index.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/avatar";
 import { Button } from "@/components/buttons";
 import {
   IconBell,
+  IconDotsVertical,
   IconLogout,
   IconMessageCircle,
   IconStar,
@@ -13,7 +14,10 @@ import {
 const AccountPage = () => {
   return (
     <div className="p-4">
-      <div className="flex flex-col gap-8 rounded-xl bg-white py-6 shadow-ring">
+      <div className="relative flex flex-col gap-8 rounded-xl bg-white py-6 shadow-ring">
+        <button className="absolute right-[18px] top-5 h-fit w-fit rounded-md p-1 transition hover:bg-slate-100">
+          <IconDotsVertical className="h-6 w-6 text-slate-600" />
+        </button>
         <div>
           <Avatar className="mx-auto mb-3 h-20 w-20">
             <AvatarImage src={import.meta.env.VITE_DEFAULT_AVATAR_PATH} />
@@ -21,17 +25,17 @@ const AccountPage = () => {
           </Avatar>
           <TypographyH3 className="text-center">roberto1223</TypographyH3>
           <TypographyP className="mb-4 text-center">12 kw.2024</TypographyP>
-          <div className="flex flex-row items-center justify-center gap-4">
+          <div className="flex flex-row items-center justify-center gap-3">
             <div className="flex flex-row items-center gap-2">
               <span className="text-sm font-medium text-slate-700">8</span>
               <IconTags className="h-6 w-6 stroke-[1.5px] text-slate-700" />
             </div>
-            <hr className="h-full w-[1px] bg-slate-200" />
+            <hr className="h-full w-4 rotate-90 bg-slate-200" />
             <div className="flex flex-row items-center gap-2">
               <span className="text-sm font-medium text-slate-700">0</span>
               <IconMessageCircle className="h-6 w-6 stroke-[1.5px] text-slate-700" />
             </div>
-            <hr className="h-full w-[1px] bg-slate-200" />
+            <hr className="h-full w-4 rotate-90 bg-slate-200" />
             <div className="flex flex-row items-center gap-2">
               <span className="text-sm font-medium text-slate-700">3</span>
               <IconBell className="h-6 w-6 stroke-[1.5px] text-slate-700" />
@@ -48,7 +52,7 @@ const AccountPage = () => {
             Zapisane
           </Button>
           <hr className="mx-auto h-[2px] w-6 bg-slate-200" />
-          <Button className="h-fit w-full border border-rose-300 bg-white px-4 py-3 font-normal text-rose-900 hover:bg-rose-100 active:bg-white">
+          <Button className="h-fit w-full border border-rose-900/20 bg-white px-4 py-3 font-normal text-rose-900 hover:bg-rose-50 active:bg-white">
             <IconLogout className="mr-2 h-5 w-5" />
             Wyloguj się
           </Button>
