12872cacfe87e7f9bb950448683fe6e731215834
Wojciech Grochecki <wojciech5522@gmail.com>
2024-05-22 10:44:50 +0200

    feat(add-deal-page): move routes from `App` component for better steps control

--- client/src/pages/AddDealPage/FormLayout.tsx
+++ client/src/pages/AddDealPage/FormLayout.tsx
@@ -1,13 +1,49 @@
 import FormContextProvider from "@/context/FormContext";
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Stepper from "./Stepper";
+import LinkStep from "./Steps/LinkStep";
+import DetailsStep from "./Steps/DetailsStep";
+import DescriptionStep from "./Steps/DescriptionStep";
+import ImagesStep from "./Steps/ImagesStep";
+import { useRef } from "react";
+
+export interface StepperRef {
+  validateAndStep: (navigateTo: string, nextStep: number) => void;
+}
+
+export const stepsLookup = [
+  "/dodaj-ogloszenie",
+  "/dodaj-ogloszenie/szczegoly",
+  "/dodaj-ogloszenie/opis",
+  "/dodaj-ogloszenie/zdjecia",
+];
 
 export default function FormLayout() {
+  const submitButtonRef = useRef<StepperRef>(null);
+
+  const onStepChange = (navigateTo: string, nextStep: number) => {
+    submitButtonRef.current?.validateAndStep(navigateTo, nextStep);
+  };
+
   return (
     <div className="flex h-fit min-h-screen flex-col gap-8 bg-white px-4 py-3">
       <FormContextProvider>
-        <Stepper />
-        <Outlet />
+        <Stepper onStepChange={onStepChange} />
+        <Routes>
+          <Route index element={<LinkStep ref={submitButtonRef} />} />
+          <Route
+            path={"szczegoly"}
+            element={<DetailsStep ref={submitButtonRef} />}
+          />
+          <Route
+            path={"opis"}
+            element={<DescriptionStep ref={submitButtonRef} />}
+          />
+          <Route
+            path={"zdjecia"}
+            element={<ImagesStep ref={submitButtonRef} />}
+          />
+        </Routes>
       </FormContextProvider>
     </div>
   );
