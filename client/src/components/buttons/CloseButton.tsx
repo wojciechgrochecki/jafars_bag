09eafd19715e6ddfa0064c4c75735adc6b07938c
Wojciech Grochecki <wojciech5522@gmail.com>
2024-05-23 20:04:58 +0200

    fix(close-button): fix `className` prop overriding defaults

--- client/src/components/buttons/CloseButton.tsx
+++ client/src/components/buttons/CloseButton.tsx
@@ -4,9 +4,13 @@ import { IconX } from "@tabler/icons-react";
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-export default function CloseButton(props: ButtonProps) {
+export default function CloseButton({ className, ...props }: ButtonProps) {
   return (
-    <Button {...props} variant={"comment"} className="h-fit w-fit p-1">
+    <Button
+      variant={"comment"}
+      className={`h-fit w-fit p-1 ${className}`}
+      {...props}
+    >
       <IconX className="h-4 w-4 text-slate-700" />
     </Button>
   );
