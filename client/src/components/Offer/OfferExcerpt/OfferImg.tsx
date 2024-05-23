a743d90700f5f3aea6cec09cfcb8b4a57c15155b
Wojciech Grochecki <wojciech5522@gmail.com>
2024-05-23 20:05:38 +0200

    feat(offer-img): add `size` prop to control `img` size

--- client/src/components/Offer/OfferExcerpt/OfferImg.tsx
+++ client/src/components/Offer/OfferExcerpt/OfferImg.tsx
@@ -1,12 +1,18 @@
 import dummyImg from "@assets/offer_picture.jpg";
 
-export default function OfferImg() {
+interface OfferImgProps {
+  size?: string;
+}
+
+export default function OfferImg({ size = "w-20 h-20" }: OfferImgProps) {
   return (
-    <div className="h-20 w-20 flex-shrink-0 cursor-pointer overflow-hidden [&:hover>img]:scale-110">
+    <div
+      className={`${size} flex-shrink-0 cursor-pointer overflow-hidden [&:hover>img]:scale-110`}
+    >
       <img
         src={dummyImg}
         alt="zdjęcie oferty"
-        className="aspect-square w-20 rounded-[4px] bg-slate-100 object-contain transition hover:scale-105"
+        className={`${size} rounded-[4px] bg-slate-100 object-contain transition hover:scale-105`}
       />
     </div>
   );
