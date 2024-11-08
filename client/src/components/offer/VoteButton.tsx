65637ed83ca4108700e379d9a077c0fb3cc53818
Wojciech Grochecki <wojciech5522@gmail.com>
2024-11-08 22:49:07 +0100

    feat(VoteButton): finish VoteButton component

--- client/src/components/offer/VoteButton.tsx
+++ client/src/components/offer/VoteButton.tsx
@@ -1,36 +1,42 @@
 import { IconThumbUp, IconThumbDown } from "@tabler/icons-react";
 import { useState } from "react";
 
-export default function VoteButton() {
-  const [selection, setSelection] = useState<"up" | "down" | undefined>(
-    undefined,
-  );
+export interface VoteButtonProps extends React.HTMLProps<HTMLDivElement> {
+  votesCount: number;
+}
+
+export default function VoteButton({
+  votesCount = 15,
+  ...props
+}: VoteButtonProps) {
+  const [selection, setSelection] = useState<number>(0);
+  const onButtonClick = (newValue: typeof selection) => {
+    setSelection(newValue);
+  };
   return (
-    <div className="flex items-center justify-center gap-1 rounded-full border border-slate-300 p-[6px]">
-      {selection !== "down" && (
-        <button
-          onClick={() => {
-            if (selection === undefined) {
-              setSelection("up");
-            }
-          }}
-        >
+    <div
+      className="flex items-center justify-center gap-1 rounded-full border border-slate-300 p-[6px]"
+      {...props}
+      onClick={(e) => e.preventDefault()}
+    >
+      {selection !== -1 && (
+        <button onClick={() => onButtonClick(1)}>
           <IconThumbUp
-            className={`h-6 w-6 stroke-1 text-green-600 hover:fill-green-200 ${selection === "up" && "fill-green-200"}`}
+            className={`h-6 w-6 stroke-1 text-green-600 hover:fill-green-200 ${selection === 1 && "fill-green-200"}`}
           />
         </button>
       )}
-      <span className={`leading h-fit text-sm`}>+93</span>
-      {selection !== "up" && (
-        <button
-          onClick={() => {
-            if (selection === undefined) {
-              setSelection("down");
-            }
-          }}
-        >
+      <span
+        className={`leading h-fit text-sm ${selection === 1 && "-order-1"}`}
+      >
+        {votesCount + selection > 0 && "+"}
+        {votesCount + selection}
+      </span>
+
+      {selection !== 1 && (
+        <button onClick={() => onButtonClick(-1)}>
           <IconThumbDown
-            className={`h-6 w-6 stroke-1 text-red-600 hover:fill-red-200 ${selection === "down" && "fill-red-200"}`}
+            className={`h-6 w-6 stroke-1 text-red-600 hover:fill-red-200 ${selection === -1 && "fill-red-200"}`}
           />
         </button>
       )}
