2bedb271b7e61546447473e190a253e43099ba7a
Wojciech Grochecki <wojciech5522@gmail.com>
2024-11-12 22:53:28 +0100

    chore(deps): update dependencies

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: { open: true },
   plugins: [react()],
   resolve: {
     alias: {
